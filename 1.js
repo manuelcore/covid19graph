6501e3134cc82b234c7890dd702ad616e44f8fea
manuelcore <manuelcore@yahoo.com>
2020-05-17 23:44:11 +0300

    Add files via upload

--- 1.js
+++ 1.js
@@ -1,6 +1,7 @@
 var dataHeatmap = [];//date, [[lat,lon],[lat,lon],...]
-
+var markerArray = [];
 function resetRange() {
+	markerArray = [];
 	$( "#myRange" ).prop( "disabled", true );
 	document.getElementById("demo").innerHTML='0';
 	try{
@@ -12,53 +13,63 @@ function resetRange() {
 		console.log(e);
 	}
 }
+function centerMap(){
+	//var bounds = L.latLngBounds(markerArray);
+	var markerBounds = L.latLngBounds(markerArray);
+  mymap.fitBounds(markerBounds);
+}
+
 function processCSVData(allText) {
 	resetRange();
 	dataHeatmap = [];//clear old values
-    var allCSVlines = allText.split(/\r\n|\n/);
+    var allCSVlines = allText.split(/\r\n|\n/);//split rows
     
-	//var lines=[];
-	
 	$.each( allCSVlines, function( index, value ){
-    var splitdata = value.split(',');
-	//input: judet/tara, data (format:day/month/year), carantinati, infectati, decedati, vindecati
+    var splitdata = value.split(',');//split one row
+	//input: county/country, data (format:day/month/year), carantinati, infectati, decedati, vindecati
 	//         0                   1                  2           3         4           5
 	var targetColumn = 2;//carantinati - change here for different column
 	//output lat,lon,carantinati
 	var lat=0,lon=0;
 	$.each( coordCountyState, function( i1, v1 ){
-		if(v1[0]==splitdata[0]){lat=v1[1];lon=v1[2];}
+		if(v1[0]==splitdata[0]){
+			lat=v1[1];lon=v1[2];
+			markerArray.push(L.latLng([lat, lon]));//used to automatically detect map bounds
+			return false;//found coords exit loop
+			}
 	});
 	
-	
+	//add all to heatmap and construct per day data
     for (var i=0;i<parseInt(splitdata[targetColumn], 10);i++)
 	{
 		heat.addLatLng({lat: lat, lng: lon});//default add all data to map
 		
 		//process data by day
-		//find existing date
+		//1. find existing date & add to existing if found
 		var found=0;
 		$.each( dataHeatmap, function( i2, v2 ){
-			if(v2[0]==splitdata[1]){v2[1].push(L.latLng(lat, lon));found=1;}
+			if(v2[0]==splitdata[1]){
+				v2[1].push(L.latLng(lat, lon));
+				found=1;}
 		});
-		//add if new date
-		//if(found==0)dataHeatmap.push([splitdata[1],[lat, lon]] );
+		//2. add if new date
 		if(found==0)dataHeatmap.push([splitdata[1],[L.latLng(lat, lon)]] );
 	}
 	});
-   
+   centerMap();
     console.log(dataHeatmap);
 	//return dataHeatmap;
 }
 
 function myFunction() {
-	alert("1. load data from csv file (coma separated values)\n" + "2. columns are: Judet(as found in judcoord.js) or country (https://developers.google.com/public-data/docs/canonical/countries_csv) , date (format:day/month/year), number");
+	alert("1. load data from csv file (coma separated values)\n" + "2. columns are: Judet(as found in judcoord.js) or country (https://developers.google.com/public-data/docs/canonical/countries_csv) , date (format:day/month/year), number\n"+
+	"3. all the data will be placed in the heatmap; to select day use checkbox and slider");
     //document.getElementById("demo").style.color = "red";
 	//heat.addLatLng({lat: 44.859763, lng: 24.889526});
 }
 
 
-//init leaflet map
+//init leaflet map with default zoom
 var mymap = L.map('mapid').setView([46.206448, 24.884033], 7);
 
 	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -137,7 +148,7 @@ slider.oninput = function() {
   
   if (this.value<dataHeatmap.length)
   {heat.setLatLngs(dataHeatmap[this.value][1]); heat.redraw(); document.getElementById("demoday").innerHTML = dataHeatmap[this.value][0];}
-//else console.log("no data");
+else console.log("no data");
 }
 
 $('#perday').on('change',function(){
